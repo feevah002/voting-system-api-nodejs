c6468c2e9e8ba18f9323dab74fd8473e4b7c0b7b
feevah002 <eikenna58@gmail.com>
2023-01-06 23:23:16 +0100

    .

--- app.js
+++ app.js
@@ -14,42 +14,7 @@ async function main (){
 
 app.use(express.urlencoded({extended:true}))
 app.use(require("./voting/route"))
-// let posts =[{
-//   president:{
-//     jane:0,
-//     diva:0,
-//   },
-//   vicePresident:{
-//     john:0,
-//     doe:0,
-//   },
-// }]
-// app.get("/poll", (req,res)=>{
-  
-//   const data = poll
-//   res.status(200).json({
-//     status:true,
-//     data:data
-//   })
-// })
-// app.post("/poll", (req,res)=>{
-//   try{
-//     const userVotes = req.body
-//     const votes = Object.entries(userVotes).map(key=> 
-//       posts.map(post=>
-//           post[key[0]][key[1]]++
-//         )
-//     )
-//     res.status(200).json({
-//       posts
-//     })
-//   } catch(err){
-//     res.status(500).json({
-//       err:err.message
-//     })
-//   }
 
-// })
 
 const port = 3000
 
